ba4877c1f38ed38c814580665feb6acd5e0c1119
KC Kai <kirstenckai@gmail.com>
2020-09-05 18:29:39 -0700

    remove api and separate info card component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from "react";
 import headerImg from "./assets/pattern-bg.png";
 import "./App.css";
-import axios from "axios";
+
+import InfoCard from "./InfoCard.js";
 
 function App() {
-  axios
-    .get("api/v1?apiKey=at_wOqGYbIdswsSjQiu9FcX7QL6VDxFx")
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-
   return (
     <div className="App">
       <img src={headerImg} alt="purple background" />
@@ -22,16 +14,7 @@ function App() {
         placeholder="Search for any IP address or domain"
       ></input>
       <button type="submit">></button>
-      <div>
-        <h4>IP Address</h4>
-        <p>192.212.174.101</p>
-        <h4>Location</h4>
-        <p>Brooklyn, NY 10001</p>
-        <h4>Timezone</h4>
-        <p>UTC-5:00</p>
-        <h4>ISP</h4>
-        <p>SpaceX Starlink</p>
-      </div>
+      <InfoCard />
     </div>
   );
 }
